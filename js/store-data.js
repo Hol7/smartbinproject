6560e59195c52b43bdf9dd47082e1cf5fae42a2d
Hol7 <40841286+Hol7@users.noreply.github.com>
2020-04-18 23:24:41 +0100

    Add files via upload

---
+++ js/store-data.js
@@ -0,0 +1,686 @@
+let stores = [{
+    "recommendation": {},
+    "storeNumber": "5758-13907",
+    "id": "15051",
+    "name": "cadjehoun airport,Benin",
+    "phoneNumber": "+229-65-98-6714",
+    "coordinates": {
+        "latitude": 6.353498586,
+        "longitude": 2.384331796
+    },
+    "regulations": [],
+    "address": {
+        "streetAddressLine1": "BAC N1",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "cotonou",
+        "countrySubdivisionCode": "CTN",
+        "countryCode": "BEN",
+        "postalCode": "902113301"
+    },
+    "timeZoneInfo": {
+        "currentTimeOffset": -420,
+        "windowsTimeZoneId": "",
+        "olsonTimeZoneId": "UTC+1 (WAT)"
+    },
+    "brandName": "AIRPORT",
+    "ownershipTypeCode": "CO",
+    "open": true,
+    "openStatusText": "Open until 00:00 PM",
+    "addressLines": ["BAC N1. AIRPORT.", "COTONOU, BENIN"],
+    "mop": {
+        "ready": false,
+        "wait": null
+    },
+    "schedule": [{
+        "dayName": "Today",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Tomorrow",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Monday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Tuesday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Wednesday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Thursday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Friday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }],
+    "features": [{
+        "code": "WA",
+        "name": "Oven-warmed Food"
+    }, {
+        "code": "VS",
+        "name": "Verismo"
+    }, {
+        "code": "CD",
+        "name": "Mobile Payment"
+    }, {
+        "code": "DR",
+        "name": "Digital Rewards"
+    }, {
+        "code": "LB",
+        "name": "LaBoulange"
+    }, {
+        "code": "GO",
+        "name": "Google Wi-Fi"
+    }, {
+        "code": "XO",
+        "name": "Mobile Order and Pay"
+    }, {
+        "code": "MX",
+        "name": "Music Experience"
+    }, {
+        "code": "NB",
+        "name": "Nitro Cold Brew"
+    }, {
+        "code": "BE",
+        "name": "Blonde Espresso"
+    }, {
+        "code": "LU",
+        "name": "Lunch"
+    }, {
+        "code": "PS",
+        "name": "Playbook Store System"
+    }, {
+        "code": "WC",
+        "name": "Wireless Charging"
+    }, {
+        "code": "UE",
+        "name": "tbd - Uber Eats"
+    }],
+    "slug": "COTONOU-AIRPORT-u"
+}, {
+    "recommendation": {},
+    "storeNumber": "5758-13907",
+    "id": "15051",
+    "name": "Centre National Hospitalier universitaire,Benin",
+    "phoneNumber": "+229-65-98-6714",
+    "coordinates": {
+        "latitude": 6.355051,
+        "longitude": 2.410072
+    },
+    "regulations": [],
+    "address": {
+        "streetAddressLine1": "BAC N2",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "cotonou",
+        "countrySubdivisionCode": "CTN",
+        "countryCode": "BEN",
+        "postalCode": "902113301"
+    },
+    "timeZoneInfo": {
+        "currentTimeOffset": -420,
+        "windowsTimeZoneId": "",
+        "olsonTimeZoneId": "UTC+1 (WAT)"
+    },
+    "brandName": "CNHU",
+    "ownershipTypeCode": "CO",
+    "open": true,
+    "openStatusText": "Open until 00:00 PM",
+    "addressLines": ["BAC N2. CNHU.", "COTONOU, BENIN"],
+    "mop": {
+        "ready": false,
+        "wait": null
+    },
+    "schedule": [{
+        "dayName": "Today",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Tomorrow",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Monday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Tuesday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Wednesday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Thursday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Friday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }],
+    "features": [{
+        "code": "WA",
+        "name": "Oven-warmed Food"
+    }, {
+        "code": "VS",
+        "name": "Verismo"
+    }, {
+        "code": "CD",
+        "name": "Mobile Payment"
+    }, {
+        "code": "DR",
+        "name": "Digital Rewards"
+    }, {
+        "code": "LB",
+        "name": "LaBoulange"
+    }, {
+        "code": "GO",
+        "name": "Google Wi-Fi"
+    }, {
+        "code": "XO",
+        "name": "Mobile Order and Pay"
+    }, {
+        "code": "MX",
+        "name": "Music Experience"
+    }, {
+        "code": "NB",
+        "name": "Nitro Cold Brew"
+    }, {
+        "code": "BE",
+        "name": "Blonde Espresso"
+    }, {
+        "code": "LU",
+        "name": "Lunch"
+    }, {
+        "code": "PS",
+        "name": "Playbook Store System"
+    }, {
+        "code": "WC",
+        "name": "Wireless Charging"
+    }, {
+        "code": "UE",
+        "name": "tbd - Uber Eats"
+    }],
+    "slug": "CNHU-u"
+}, {
+    "recommendation": {},
+    "storeNumber": "5758-13907",
+    "id": "15051",
+    "name": "PLace de l'etoile rouge,Benin",
+    "phoneNumber": "+229-65-98-6714",
+    "coordinates": {
+        "latitude": 6.371515,
+        "longitude": 2.409821
+    },
+    "regulations": [],
+    "address": {
+        "streetAddressLine1": "BAC N3",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "cotonou",
+        "countrySubdivisionCode": "CTN",
+        "countryCode": "BEN",
+        "postalCode": "902113301"
+    },
+    "timeZoneInfo": {
+        "currentTimeOffset": -420,
+        "windowsTimeZoneId": "",
+        "olsonTimeZoneId": "UTC+1 (WAT)"
+    },
+    "brandName": "ETOILE ROUGE",
+    "ownershipTypeCode": "CO",
+    "open": true,
+    "openStatusText": "Open until 00:00 PM",
+    "addressLines": ["BAC N3. ETOILE ROUGE.", "COTONOU, BENIN"],
+    "mop": {
+        "ready": false,
+        "wait": null
+    },
+    "schedule": [{
+        "dayName": "Today",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Tomorrow",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Monday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Tuesday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Wednesday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Thursday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Friday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }],
+    "features": [{
+        "code": "WA",
+        "name": "Oven-warmed Food"
+    }, {
+        "code": "VS",
+        "name": "Verismo"
+    }, {
+        "code": "CD",
+        "name": "Mobile Payment"
+    }, {
+        "code": "DR",
+        "name": "Digital Rewards"
+    }, {
+        "code": "LB",
+        "name": "LaBoulange"
+    }, {
+        "code": "GO",
+        "name": "Google Wi-Fi"
+    }, {
+        "code": "XO",
+        "name": "Mobile Order and Pay"
+    }, {
+        "code": "MX",
+        "name": "Music Experience"
+    }, {
+        "code": "NB",
+        "name": "Nitro Cold Brew"
+    }, {
+        "code": "BE",
+        "name": "Blonde Espresso"
+    }, {
+        "code": "LU",
+        "name": "Lunch"
+    }, {
+        "code": "PS",
+        "name": "Playbook Store System"
+    }, {
+        "code": "WC",
+        "name": "Wireless Charging"
+    }, {
+        "code": "UE",
+        "name": "tbd - Uber Eats"
+    }],
+    "slug": "ETOILE-ROUGE-u"
+}, {
+    "recommendation": {},
+    "storeNumber": "5758-13907",
+    "id": "15051",
+    "name": "Vedoko CFAO motors CHEZ MOI,Cotonou",
+    "phoneNumber": "+229-65-98-67-14",
+    "coordinates": {
+        "latitude": 6.375097,
+        "longitude": 2.386252
+    },
+    "regulations": [],
+    "address": {
+        "streetAddressLine1": "BAC N4",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "cotonou",
+        "countrySubdivisionCode": "CTN",
+        "countryCode": "BEN",
+        "postalCode": "902113301"
+    },
+    "timeZoneInfo": {
+        "currentTimeOffset": -420,
+        "windowsTimeZoneId": "",
+        "olsonTimeZoneId": "UTC+1 (WAT)"
+    },
+    "brandName": "CHEZ MOI",
+    "ownershipTypeCode": "CO",
+    "open": true,
+    "openStatusText": "Open until 6:00 PM",
+    "addressLines": ["BAC N4, CHEZ FABRONE BITITI.", "COTONOU, BENIN"],
+    "mop": {
+        "ready": false,
+        "wait": null
+    },
+    "schedule": [{
+        "dayName": "Today",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Tomorrow",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Monday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Tuesday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Wednesday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Thursday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Friday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }],
+    "features": [{
+        "code": "WA",
+        "name": "Oven-warmed Food"
+    }, {
+        "code": "VS",
+        "name": "Verismo"
+    }, {
+        "code": "CD",
+        "name": "Mobile Payment"
+    }, {
+        "code": "DR",
+        "name": "Digital Rewards"
+    }, {
+        "code": "LB",
+        "name": "LaBoulange"
+    }, {
+        "code": "GO",
+        "name": "Google Wi-Fi"
+    }, {
+        "code": "XO",
+        "name": "Mobile Order and Pay"
+    }, {
+        "code": "MX",
+        "name": "Music Experience"
+    }, {
+        "code": "NB",
+        "name": "Nitro Cold Brew"
+    }, {
+        "code": "BE",
+        "name": "Blonde Espresso"
+    }, {
+        "code": "LU",
+        "name": "Lunch"
+    }, {
+        "code": "PS",
+        "name": "Playbook Store System"
+    }, {
+        "code": "WC",
+        "name": "Wireless Charging"
+    }, {
+        "code": "UE",
+        "name": "tbd - Uber Eats"
+    }],
+    "slug": "CHEZ-MOI-u"
+}, {
+    "recommendation": {},
+    "storeNumber": "5758-13907",
+    "id": "15051",
+    "name": "Stade de l'amitie,Benin",
+    "phoneNumber": "+229-65-98-67-14",
+    "coordinates": {
+        "latitude": 6.385674,
+        "longitude": 2.379537
+    },
+    "regulations": [],
+    "address": {
+        "streetAddressLine1": "BAC N5",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "cotonou",
+        "countrySubdivisionCode": "CTN",
+        "countryCode": "BEN",
+        "postalCode": "902113301"
+    },
+    "timeZoneInfo": {
+        "currentTimeOffset": -420,
+        "windowsTimeZoneId": "",
+        "olsonTimeZoneId": "UTC+1 (WAT)"
+    },
+    "brandName": "STADE",
+    "ownershipTypeCode": "CO",
+    "open": true,
+    "openStatusText": "Open until 00:00 PM",
+    "addressLines": ["BAC N5, Stade de l'amitie.", "COTONOU, BENIN"],
+    "mop": {
+        "ready": false,
+        "wait": null
+    },
+    "schedule": [{
+        "dayName": "Today",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Tomorrow",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Monday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Tuesday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Wednesday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Thursday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Friday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }],
+    "features": [{
+        "code": "WA",
+        "name": "Oven-warmed Food"
+    }, {
+        "code": "VS",
+        "name": "Verismo"
+    }, {
+        "code": "CD",
+        "name": "Mobile Payment"
+    }, {
+        "code": "DR",
+        "name": "Digital Rewards"
+    }, {
+        "code": "LB",
+        "name": "LaBoulange"
+    }, {
+        "code": "GO",
+        "name": "Google Wi-Fi"
+    }, {
+        "code": "XO",
+        "name": "Mobile Order and Pay"
+    }, {
+        "code": "MX",
+        "name": "Music Experience"
+    }, {
+        "code": "NB",
+        "name": "Nitro Cold Brew"
+    }, {
+        "code": "BE",
+        "name": "Blonde Espresso"
+    }, {
+        "code": "LU",
+        "name": "Lunch"
+    }, {
+        "code": "PS",
+        "name": "Playbook Store System"
+    }, {
+        "code": "WC",
+        "name": "Wireless Charging"
+    }, {
+        "code": "UE",
+        "name": "tbd - Uber Eats"
+    }],
+    "slug": "STADE-u"
+}, {
+    "recommendation": {},
+    "storeNumber": "+229-21-32-21-85",
+    "id": "15051",
+    "name": "INSTITUT CERCO,Benin",
+    "phoneNumber": "+229-21-32-42-20",
+    "coordinates": {
+        "latitude": 6.368956,
+        "longitude": 2.416395
+    },
+    "regulations": [],
+    "address": {
+        "streetAddressLine1": "BAC N6",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "cotonou",
+        "countrySubdivisionCode": "CTN",
+        "countryCode": "BEN",
+        "postalCode": "902113301"
+    },
+    "timeZoneInfo": {
+        "currentTimeOffset": -420,
+        "windowsTimeZoneId": "",
+        "olsonTimeZoneId": "UTC+1 (WAT)"
+    },
+    "brandName": "GROUPE CERCO",
+    "ownershipTypeCode": "CO",
+    "open": true,
+    "openStatusText": "Open until 6:00 PM",
+    "addressLines": ["BAC N6, Institut cerco.", "COTONOU, BENIN"],
+    "mop": {
+        "ready": false,
+        "wait": null
+    },
+    "schedule": [{
+        "dayName": "Today",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Tomorrow",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Monday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Tuesday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Wednesday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Thursday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Friday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }],
+    "features": [{
+        "code": "WA",
+        "name": "Oven-warmed Food"
+    }, {
+        "code": "VS",
+        "name": "Verismo"
+    }, {
+        "code": "CD",
+        "name": "Mobile Payment"
+    }, {
+        "code": "DR",
+        "name": "Digital Rewards"
+    }, {
+        "code": "LB",
+        "name": "LaBoulange"
+    }, {
+        "code": "GO",
+        "name": "Google Wi-Fi"
+    }, {
+        "code": "XO",
+        "name": "Mobile Order and Pay"
+    }, {
+        "code": "MX",
+        "name": "Music Experience"
+    }, {
+        "code": "NB",
+        "name": "Nitro Cold Brew"
+    }, {
+        "code": "BE",
+        "name": "Blonde Espresso"
+    }, {
+        "code": "LU",
+        "name": "Lunch"
+    }, {
+        "code": "PS",
+        "name": "Playbook Store System"
+    }, {
+        "code": "WC",
+        "name": "Wireless Charging"
+    }, {
+        "code": "UE",
+        "name": "tbd - Uber Eats"
+    }],
+    "slug": "INSTITUT-CERCO-BENIN-u"
+}
+]
